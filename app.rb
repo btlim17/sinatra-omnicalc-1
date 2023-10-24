8c9a5b788e06619098b3f412cd287cea85c06791
btlim17 <146046964+btlim17@users.noreply.github.com>
2023-10-24 17:37:41 +0000

    random_number_update

--- app.rb
+++ app.rb
@@ -38,13 +38,18 @@ get("/payment/results") do
   @rate = params.fetch("apr_value").to_f
   @n = params.fetch("num_years").to_f
   @pv = params.fetch("principal_value").to_f
-  @payment = ((@rate/100)* @pv) / (1-((1+ @rate)**(-@n)))
+  @payment = (((@rate/1200)* @pv) / (1-((1+ (@rate/1200))**(-@n*12))))
   erb(:payment_results)
 end
 
 #random-new
 get("/random/new") do
-
+  erb(:rand_new)
 end
 
-
+get("/random/results") do
+  @min = params.fetch("min_value").to_f
+  @max = params.fetch("max_value").to_f
+  @number = rand(@min..@max)
+  erb(:rand_results)
+end
