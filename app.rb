fd89e87625f7931f6716b4f4d2b05f47685874d2
btlim17 <146046964+btlim17@users.noreply.github.com>
2023-10-24 05:11:50 +0000

    omnicalc1_v1

--- app.rb
+++ app.rb
@@ -1,9 +1,46 @@
 require "sinatra"
 require "sinatra/reloader"
 
+#Home page
 get("/") do
   "
   <h1>Welcome to your Sinatra App!</h1>
   <p>Define some routes in app.rb</p>
   "
 end
+
+#Square-new
+get("/square/new") do
+  erb(:square_new)
+
+end
+
+get("/square/results") do
+  number=params.fetch("square_number").to_f
+  squared_num = number ** 2
+  pp number
+  pp squared_num
+end
+
+
+#Square-root-new
+get("/square_root/new") do
+
+
+
+end
+
+#payment-new
+get("/payment/new") do
+
+
+
+end
+
+#random-new
+get("/random/new") do
+
+
+end
+
+#results
